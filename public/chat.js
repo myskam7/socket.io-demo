31ab2821a8d163bc36bd58a3e53898f4579eb20f
myskam7 <mksmflppv98@gmail.com>
2018-06-12 18:20:22 -0400

    id

--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,37 @@
 //Make Connection
 
 var socket = io.connect('http://localhost:8000');
+
+//Query DOM (store these ELEMENTS inside VARIABLES )
+var message = document.getElementById('message'),
+    handle = document.getElementById('handle'),
+    btn = document.getElementById('send'),
+    output = document.getElementById('output'),
+    feedback = document.getElementById('feedback');
+
+
+
+
+//Emit Event
+
+btn.addEventListener('click', function(){
+    //socket.emit('type of message', 'message')
+    socket.emit('chat',{
+        message: message.value,
+        handle: handle.value
+    }); 
+});
+
+message.addEventListener('keypress', function(){
+   socket.emit('typing', handle.value); //when client is typing other clients can see clients handler
+});
+
+// Listen for Events
+socket.on('chat', function(data){
+   output.innerHTML += "<p><strong>" + data.handle + "; </strong>" + data.message + "</p>";
+   feedback.innerHTML = "";
+});
+
+socket.on('typing', function(data){
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message...' + '</p></em>;'
+})
--- server.js
+++ server.js
@@ -19,4 +19,13 @@ var io = socket(server);
 
 io.on('connection', function(socket){ //io.on(event, cb func){}
    console.log('connected socket: ', socket.id) // socket gives unique id to every user
+
+   socket.on('chat', function(data){
+       //once msg is sent io.sockets.emit(type of msg, data) will send it to all the clients
+       io.sockets.emit('chat', data);
+   });
+
+   socket.on('typing', function(data){
+       socket.broadcast.emit('typing', data); //renders 'typing to all clients except to the client typing
+   });
 });
